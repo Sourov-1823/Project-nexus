03a2721ee0ee1b18de5de5a8d04d45686f666e6e
Sourov Debnath <143125587+Sourov-1823@users.noreply.github.com>
2024-07-30 18:54:27 +0530

    index.js

--- login_page/index.js
+++ login_page/index.js
@@ -1,1 +1,12 @@
-
+const LoginForm = document.querySelector('.LoginForm');
+const SignupForm = document.querySelector('.SignupForm');
+const LoginLink = document.querySelector('.LoginLink');
+const RegisterLink = document.querySelector('.RegisterLink');
+RegisterLink.onclick=()=>{
+    SignupForm.classList.add('active');
+    LoginForm.classList.add('active');
+}
+LoginLink.onclick=()=>{
+    SignupForm.classList.remove('active');
+    LoginForm.classList.remove('active');
+}
