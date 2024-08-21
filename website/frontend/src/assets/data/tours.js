abb9acd7f49752f8969b77207378cef93295dd3d
Sourov Debnath <143125587+Sourov-1823@users.noreply.github.com>
2024-08-21 20:06:03 +0530

    Update tours.js

--- website/frontend/src/assets/data/tours.js
+++ website/frontend/src/assets/data/tours.js
@@ -1,10 +1,10 @@
-import tourImg01 from "../images/tour-img01.jpg";
-import tourImg02 from "../images/tour-img02.jpg";
-import tourImg03 from "../images/tour-img03.jpg";
-import tourImg04 from "../images/tour-img04.jpg";
-import tourImg05 from "../images/tour-img05.jpg";
-import tourImg06 from "../images/tour-img06.jpg";
-import tourImg07 from "../images/tour-img07.jpg";
+import tourImg01 from "../tour-img01.jpg";
+import tourImg02 from "../tour-img02.jpg";
+import tourImg03 from "../tour-img03.jpg";
+import tourImg04 from "../tour-img04.jpg";
+import tourImg05 from "../tour-img05.jpg";
+import tourImg06 from "../tour-img06.jpg";
+import tourImg07 from "../tour-img07.jpg";
 
 const tours = [
   {
